eb67980626d7a274c9e4361ddba1f92b5c46eb90
Sergey Voroshilin <medulla_261@mail.ru>
2017-05-06 16:10:41 +0300

    add properties of entities to model

--- ModelUnit/Entities/Entity.cs
+++ ModelUnit/Entities/Entity.cs
@@ -5,5 +5,10 @@ namespace WildNature.ModelUnit.Entities
 	abstract class Entity
 	{
 		public Point Location { get; set; }
+
+		public virtual void SetLocation(int x, int y)
+		{
+			Location = new Point(x, y);
+		}
 	}
 }
--- ModelUnit/Model.cs
+++ ModelUnit/Model.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using WildNature.ModelUnit.Entities;
+using WildNature.ModelUnit.Entities.Alives;
 using WildNature.ModelUnit.Entities.Alives.Monsters;
 using WildNature.ModelUnit.Entities.Bonuses;
 using WildNature.ModelUnit.Entities.Obstacles;
@@ -16,20 +15,75 @@ namespace WildNature.ModelUnit
 		private readonly int _fieldWidth;
 		private readonly int _fieldHeight;
 		private bool _inGame;
+		private List<Entity> _entities;
+
+		public IEnumerable<AliveEntity> Alives
+		{
+			get { return _entities.OfType<AliveEntity>(); }
+		}
+
+		public IEnumerable<IBonus> Bonuses
+		{
+			get { return _entities.OfType<IBonus>(); }
+		}
+
+		public IEnumerable<Obstacle> Obstacles
+		{
+			get { return _entities.OfType<Obstacle>(); }
+		}
 
 		public Model(int fieldWidth, int fieldHeight)
 		{
 			_fieldWidth = fieldWidth;
 			_fieldHeight = fieldHeight;
 			_inGame = false;
+			_entities = new List<Entity>();
 		}
 
 		public void StartGame()
 		{
+			LoadEntities();
 			while (_inGame)
 			{
 				MakeStep();
 				Thread.Sleep(10);
+			}
+		}
+
+		private void LoadEntities()
+		{
+			LoadObstacles();
+			LoadMonsters();
+			LoadBonuses();
+		}
+
+		private void LoadObstacles()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				_entities.Add(CreateEntity<Tree>());
+				_entities.Add(CreateEntity<Pit>());
+				_entities.Add(CreateEntity<Stone>());
+			}
+		}
+
+		private void LoadMonsters()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				_entities.Add(CreateEntity<Bear>());
+				_entities.Add(CreateEntity<Snake>());
+				_entities.Add(CreateEntity<Wolf>());
+			}
+		}
+
+		private void LoadBonuses()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				_entities.Add(CreateEntity<Cherry>());
+				_entities.Add(CreateEntity<Apple>());
+				_entities.Add(CreateEntity<Raspberry>());
 			}
 		}
 
